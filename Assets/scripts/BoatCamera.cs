c30c8889ce21e4255d8300c060c61e1515a58a34
Ian Crawford <Ian@CrawfishIII>
2017-12-02 01:40:02 -0800

    Various fixes

--- Assets/scripts/BoatCamera.cs
+++ Assets/scripts/BoatCamera.cs
@@ -10,11 +10,14 @@ public class BoatCamera : MonoBehaviour
     [SerializeField]
     private Vector3 offset = Vector3.back * 10f;
     [SerializeField]
+    private Vector3 localOffset = Vector3.zero;
+    [SerializeField]
     private float velocityScale = 1.0f;
 
     private void LateUpdate()
     {
         transform.position = boatRigidbody.transform.position + offset;
+        transform.position += boatRigidbody.transform.TransformDirection(localOffset);
         transform.position += (Vector3)boatRigidbody.velocity * velocityScale;
     }
 }
