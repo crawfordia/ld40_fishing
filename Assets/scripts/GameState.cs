b0d0dd92c15a9b837152fad3c5ff6f1e7c1b0e48
Ian Crawford <Ian@CrawfishIII>
2017-12-02 00:47:52 -0800

    Stub game state and UI.

---
+++ Assets/scripts/GameState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameState : MonoBehaviour
+{
+    [HideInInspector]
+    public int Fish;
+
+    [SerializeField]
+    private Text fishText;
+
+    // Use this for initialization
+    void Start()
+    {
+        Fish = 0;
+        fishText.text = Fish.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
