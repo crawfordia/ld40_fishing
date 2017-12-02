8e8a2bac63a008f2b159865d93e2a68d67858ea3
Tim Slippy, voxelv <slippytim@gmail.com>
2017-12-02 02:45:02 -0800

    sinkChance algorithm

--- Assets/scripts/GameState.cs
+++ Assets/scripts/GameState.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Assets.scripts.boats;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,16 +12,54 @@ public class GameState : MonoBehaviour
     [SerializeField]
     private Text fishText;
 
+    [SerializeField]
+    private float sinkChance;
+
+    [SerializeField]
+    private float storminess;
+
+    enum BoatType {
+        boat1x2,
+        boat2x2,
+        boat2x3,
+        boat2x4
+    }
+    BoatType currentBoatType = BoatType.boat1x2;
+
+    Dictionary<BoatType, BaseBoat> boats = new Dictionary<BoatType, BaseBoat>();
+
     // Use this for initialization
     void Start()
     {
+        sinkChance = 0.0f;
+        storminess = 0.0f;
         Fish = 0;
         fishText.text = Fish.ToString();
+
+        boats.Add(BoatType.boat1x2, new BaseBoat(0.1f, 1, 2));
+        boats.Add(BoatType.boat2x2, new BaseBoat(0.2f, 2, 2));
+        boats.Add(BoatType.boat2x3, new BaseBoat(0.3f, 2, 3));
+        boats.Add(BoatType.boat2x4, new BaseBoat(0.4f, 2, 4));
     }
 
     // Update is called once per frame
     void Update()
     {
+        updateSinkChance();
 
+        if(sinkChance >= 1.0f) {
+            Debug.Log("SUNK!");
+        }
+    }
+
+    void updateSinkChance()
+    {
+        BaseBoat currentBoat = boats[currentBoatType];
+        // Algorithm:
+        sinkChance = Mathf.Max(0.0f, 
+            -(currentBoat.stabilityIncrease) 
+            + Random.Range(0, storminess) 
+            + (Fish / currentBoat.fishCapacity)
+            );
     }
 }
---
+++ Assets/scripts/boats/BaseBoat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Assets.scripts.boats
+{
+    public class BaseBoat
+    {
+        public float stabilityIncrease;
+        public int fishCapacity;
+        public int cols, rows;
+
+        public BaseBoat(float stability, int cols, int rows)
+        {
+            stabilityIncrease = stability;
+            this.cols = cols;
+            this.rows = rows;
+            fishCapacity = cols * rows;
+        }
+    }
+}
