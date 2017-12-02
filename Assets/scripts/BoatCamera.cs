d49515e53d1fc3f824b45daf8d82d359b9b7b9f0
Ian Crawford <Ian@CrawfishIII>
2017-12-01 20:47:55 -0800

    Add simple follow camera for boat.

---
+++ Assets/scripts/BoatCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D boatRigidbody;
+
+    [SerializeField]
+    private Vector3 offset = Vector3.back * 10f;
+    [SerializeField]
+    private float velocityScale = 1.0f;
+
+    private void LateUpdate()
+    {
+        transform.position = boatRigidbody.transform.position + offset;
+        transform.position += (Vector3)boatRigidbody.velocity * velocityScale;
+    }
+}
