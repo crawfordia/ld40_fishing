be13feff409cfd3be19656aef11706deda3365a2
Tim Slippy, voxelv <slippytim@gmail.com>
2017-12-01 22:51:31 -0800

    scrolling ocean

---
+++ Assets/scripts/OceanTextureOffset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class OceanTextureOffset : MonoBehaviour {
+
+    public GameObject boat;
+    public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rend.material.mainTextureOffset = boat.transform.position * 0.1f;
+	}
+}
