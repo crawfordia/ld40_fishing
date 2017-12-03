9be47c474d26acbe43b40222457993f147e50cd2
Tim Slippy, voxelv <slippytim@gmail.com>
2017-12-03 14:46:02 -0800

    boat wake particles!

---
+++ Assets/scripts/particlesController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particlesController : MonoBehaviour {
+
+    public GameObject boat;
+
+    public GameObject[] particles;
+
+    private void Start()
+    {
+        StartCoroutine(playParticles());
+    }
+
+    public IEnumerator playParticles()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+
+            bool boatMoving = boat.GetComponent<Rigidbody2D>().velocity.magnitude > 0.5f;
+
+
+            foreach (GameObject go in particles)
+            {
+                ParticleSystem ps = go.GetComponent<ParticleSystem>();
+                if(boatMoving)
+                {
+                    ps.Play();
+                }
+            }
+        }
+    }
+}
