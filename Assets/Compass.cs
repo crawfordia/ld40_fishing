4115a24941a9f26d579357c55397220b7fd75d90
Ian Crawford <Ian@CrawfishIII>
2017-12-02 16:06:40 -0800

    Added compass.

---
+++ Assets/Compass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    [SerializeField]
+    private Transform from;
+    [SerializeField]
+    private Transform to;
+
+    private void LateUpdate()
+    {
+        transform.right = (from.position - to.position).normalized;
+    }
+}
